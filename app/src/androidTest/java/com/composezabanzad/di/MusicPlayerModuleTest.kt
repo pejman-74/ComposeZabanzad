e5626d9d503d514cb35a1a56975e097b12ec51b7
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-07-12 20:33:02 +0430

    Fix annotation issue

--- app/src/androidTest/java/com/composezabanzad/di/MusicPlayerModuleTest.kt
+++ app/src/androidTest/java/com/composezabanzad/di/MusicPlayerModuleTest.kt
@@ -2,12 +2,14 @@ package com.composezabanzad.di
 
 import com.composezabanzad.util.BackgroundMusicPlayer
 import com.composezabanzad.util.FakeBackgroundMusicPlayerImpl
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
 import javax.inject.Singleton
 
 @TestInstallIn(components = [SingletonComponent::class], replaces = [MusicPlayerModule::class])
+@Module
 object MusicPlayerModuleTest {
     @Singleton
     @Provides
