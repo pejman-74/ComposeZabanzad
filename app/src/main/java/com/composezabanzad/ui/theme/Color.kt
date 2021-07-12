e4f89626fe1af49f0ea96d210d6710a168dba0d7
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-07-12 10:21:29 +0430

    Add colors and font

--- app/src/main/java/com/composezabanzad/ui/theme/Color.kt
+++ app/src/main/java/com/composezabanzad/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.composezabanzad.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val lightYellow = Color(0xFFFFA000)
+val lightOrange = Color(0xFFF57C00)
+val orange = Color(0xFFFF5722)
+val lightPurple = Color(0xFFBA68C8)
+val purple = Color(0xFF883997)
--- app/src/main/java/com/composezabanzad/ui/theme/Type.kt
+++ app/src/main/java/com/composezabanzad/ui/theme/Type.kt
@@ -1,28 +1,80 @@
 package com.composezabanzad.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.composezabanzad.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+val bTitr = FontFamily(Font(R.font.titr_bold))
+
+val Typography
+    get() = Typography(
+        h1 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 96.sp,
+        ),
+        h2 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 60.sp,
+        ),
+        h3 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 48.sp,
+        ),
+        h4 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 34.sp,
+        ),
+        h5 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 24.sp,
+        ),
+        h6 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 20.sp,
+        ),
+        subtitle1 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 16.sp,
+        ),
+        subtitle2 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 14.sp,
+        ),
+        body1 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 16.sp,
+        ),
+        body2 = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 14.sp,
+        ),
+        button = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 14.sp,
+        ),
+        caption = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 12.sp,
+        ),
+        overline = TextStyle(
+            color = Color.White,
+            fontFamily = bTitr,
+            fontSize = 10.sp,
+        )
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
-)
